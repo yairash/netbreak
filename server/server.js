7430fb725cc5cebdfc375edb69e5cfbbd4912137
yairash <yairash13@gmail.com>
2023-06-03 18:54:11 +0300

    Added azure-server + changes to server.js + test.html:

--- server/server.js
+++ server/server.js
@@ -11,11 +11,9 @@ const port = 3000;
 app.get('/', async (req, res) => {
     console.log("someone reached!");
     try {
-        const urlToFetch = req.query.externalUrl;
         // const fileName = req.query.htmlFileName;
-
+        const urlToFetch = req.query.externalUrl;
         await savePage(urlToFetch);
-
         fs.readFile('test.html', 'utf8', (err, fileContent) => {
             if (err) {
                 console.error(err);
@@ -24,27 +22,22 @@ app.get('/', async (req, res) => {
                 res.status(200).send(fileContent);
             }
         });
-    } catch (error) {
+    }
+    catch (error) {
+        console.log(error.message);
         res.status(200).send("no externalurl provided");
     }
 
 });
 
-// express - Start the server
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
 
-
-
-// puppeteer code
-
 async function savePage(url) {
-
     const browser = await puppeteer.launch({ headless: 'new' });
     const page = await browser.newPage();
-
-    await page.goto(url, { waitUntil: 'networkidle0' });
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
     const domContent = await page.content();
     fs.writeFileSync('test.html', domContent);
 
