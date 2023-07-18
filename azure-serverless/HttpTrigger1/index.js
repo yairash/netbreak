4f17d3bb102a6c74f963e78b80eedd2145debbb8
yairash <yairash13@gmail.com>
2023-07-18 14:57:35 +0300

    updated trigger1 index

--- azure-serverless/HttpTrigger1/index.js
+++ azure-serverless/HttpTrigger1/index.js
@@ -1,7 +1,4 @@
 const puppeteer = require("puppeteer");
-const axios = require('axios');
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
 
 module.exports = async function (context, req) {
 
@@ -9,29 +6,12 @@ module.exports = async function (context, req) {
     let domContent, responseMessage;
     try {
         const urlToFetch = req.query.externalUrl;
-        // const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] });
-        // const page = await browser.newPage();
-        // await page.goto(urlToFetch, { waitUntil: 'networkidle0' });
-        // domContent = await page.content();
-        // responseMessage = domContent;
-        // await browser.close();
-
-        // await axios.get(urlToFetch)
-        //     .then(response => {
-        //         const dom = response.data;
-        //         responseMessage = dom;
-        //     })
-        //     .catch(error => {
-        //         responseMessage = error;
-        //     });
-
-        await getDOMFromURL(urlToFetch)
-            .then((dom) => {
-                responseMessage = dom.documentElement.outerHTML;
-            })
-            .catch((error) => {
-                responseMessage = error;
-            });
+        const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] });
+        const page = await browser.newPage();
+        await page.goto(urlToFetch, { waitUntil: 'domcontentloaded' });
+        domContent = await page.content();
+        responseMessage = domContent;
+        await browser.close();
 
 
     }
@@ -43,17 +23,4 @@ module.exports = async function (context, req) {
         status: 200,
         body: responseMessage
     };
-}
-
-
-function getDOMFromURL(url) {
-    return new Promise((resolve, reject) => {
-        JSDOM.fromURL(url)
-            .then((dom) => {
-                resolve(dom.window.document);
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
 }
